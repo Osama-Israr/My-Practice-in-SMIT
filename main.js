148f5a180c65383a8ff8c066dd32863245c56ed0
Osama-Israr <166290624+Osama-Israr@users.noreply.github.com>
2024-06-02 12:12:03 +0500

    June 2 Work Completed

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// const users = [ { name: 'usama', email: 'usama@gmail.com', password: '1190' } ];
+const users = [ { name: 'usama', email: 'usama@gmail.com', password: '1190' } ];
 
-// function Userconstructor(name, email, password) {
-//     this.name = name;
-//     this.email = email;
-//     this.password = password;       
-// }
+function Userconstructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;       
+}
 
-// function login() {}
+function login() {}
 
-// function signup() {}
+function signup() {}
 

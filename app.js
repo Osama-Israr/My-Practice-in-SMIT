ffde5b2fdcba03364fa9d6e07f5c186ac10fbd73
Osama-Israr <166290624+Osama-Israr@users.noreply.github.com>
2024-06-15 11:05:42 +0500

    June 15, 2024 work

--- app.js
+++ app.js
@@ -98,3 +98,92 @@ response.then(function(val) {
 // payload                                                  cors error ( default behaviour ) , no cors , self origin
 
 // API Integeration
+
+
+// let myArray = fetch('https://jsonplaceholder.typicode.com/todos').then((data) => {
+//     let myVar = data.json();
+
+//     myVar.then((data) => {
+//         console.log(data[0]);
+//     })
+// })
+
+// console.log(myArray);
+
+
+
+
+
+
+// June 15 , 2024           JavaScript Event Loop
+
+// function() {}          : Syntax Error
+
+// JavaScript first compile then interpreted program , Just in time compilation (JITC)
+
+// Hoisting Concepts
+
+// console.log(a, '==== a =====');       // undefined
+
+// var a = 'value';
+
+// console.log(window);
+
+// console.log(b);                                  // ReferenceError b is not defined
+
+// console.log(myFunc);                                // display function code
+
+// myFunc();                                           // function code running 
+
+// function myFunc() {
+//     console.log("Function Run...");
+// }
+
+
+// lexical scope
+
+// function myFunc() {
+//     console.log('run');
+// }
+
+// const result = myFunc();
+
+// console.log(result);
+
+// console.log(window);
+
+// console.log(a, '==== a =====');
+// console.log(b, '===== b =====');            // ReferenceError 
+
+// var b = 'cde';
+// let a = 'abc';
+
+
+
+// for of loop
+// const myArray = [1, 2, 3, 4, 5];
+
+// for(let i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i], i);
+// }
+
+// for(let items of myArray) {
+//     console.log(items, "Short hand method for of loop");
+// }
+
+// for(let items in myArray) {                                   // Not for array , it is used of an object
+//     console.log(items, "Short hand method for in loop");
+// }
+
+
+// for in loop    ( object )
+
+// const person = {
+//     name: 'usama',
+//     age: 21,
+//     gender: "male"
+// }
+
+// for(let keys in person) {
+//     console.log(keys);
+// }

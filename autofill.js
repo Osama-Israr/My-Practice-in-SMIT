4252f23f7b91d83a5efbbeb673f576ac6a2bddca
Osama-Israr <166290624+Osama-Israr@users.noreply.github.com>
2024-06-30 12:00:57 +0500

    Work June 30 , 2024

---
+++ autofill.js
@@ -0,0 +1,49 @@
+// document.write('JS Connected!');
+
+// Typa-Head
+
+let countryList = { 
+    p: 'akistan', i: 'ndia', n: 'ew zealand', e: 'ngland', a: 'merica'
+};
+
+const userInput = document.getElementById('input');
+const autoFill = document.getElementById('autofill');
+
+userInput.addEventListener('input', (e) => {
+    const userValue = e.target.value;
+    console.log(userValue);
+})
+
+
+
+
+
+// 4 statements
+
+// if(value) {
+
+// }
+// else {
+
+// }
+
+// value ? this : other
+
+// valie || 'usama'
+
+
+// falsii value => undefined , null , false ...
+
+// empty array is not a falsi value
+
+// short circuits (true(first) || )
+
+// const result = value && other;
+
+// ternary operators (condition ? true : false)
+
+// const myVar1 = 10000;
+
+// var myVar2 = 20000;
+
+// const result = myVar1 > 10000 ? myVar1 : myVar2
